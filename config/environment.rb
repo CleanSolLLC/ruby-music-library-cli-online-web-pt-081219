05a527260a17777fa866e077166a15bfa8eb8691
CleanSolLLC <mamark90@gmail.com>
2020-03-22 10:06:56 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,7 @@ Bundler.require
 module Concerns
 end
 
+require_relative "../lib/concerns/concerns_findable"
+require_relative "../lib/music_library_cli_methods"
+require_relative "../lib/music_library_controller"
 require_all 'lib'
